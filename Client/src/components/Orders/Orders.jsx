2fff320623c6105e3556351421fff25537ddad7a
rajgupta2399 <rajumardotnet2499@gmail.com>
2024-08-08 19:19:45 +0530

    lastChanges

--- Client/src/components/Orders/Orders.jsx
+++ Client/src/components/Orders/Orders.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import Banner from "../../assets/img_404.png";
 
 const Orders = () => {
   return (
-    <div className="flex justify-center align-middle h-[85vh]">
-      Orders Page
+    <div className="flex justify-center align-middle">
+      <div className="my-[4vw]">
+        <img src={Banner} alt="" />
+      </div>
     </div>
   );
 };
--- Server/app.js
+++ Server/app.js
@@ -42,8 +42,8 @@ app.post("/api/create-checkout-session", async (req, res) => {
       payment_method_types: ["card"],
       line_items: lineItems,
       mode: "payment",
-      success_url: "http://localhost:5173/sucess",
-      cancel_url: "http://localhost:5173/cancel",
+      success_url: "https://swiggybyrk.netlify.app/orders",
+      cancel_url: "https://swiggybyrk.netlify.app/orders",
     });
 
     res.json({ id: session.id });
